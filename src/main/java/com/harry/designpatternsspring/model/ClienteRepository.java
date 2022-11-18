023ac55880a87a243e3726890b6802c71027cdeb
harryazawa <victorlbamorim@gmail.com>
2022-11-18 15:58:36 -0300

    feat: added model folder with Cliente and Endereco and their repositories

---
+++ src/main/java/com/harry/designpatternsspring/model/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.harry.designpatternsspring.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+
+}
